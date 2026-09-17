d86821cb6e22e1b057b3e6b893fa782f0f05050a
IrvinDon <bruhguyalbert@gmail.com>
2026-09-17 23:25:50 +0100

    Delete ani-1071/chapitre-01 directory

--- ani-1071/chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++
@@ -1,6 +0,0 @@
-#include<cstdio>
-int main (){
-    printf("Nkoulou Etoga\n");
-    printf("Yaoundé\n");
-    return 0;
-}
--- ani-1071/chapitre-01/exo2-sans_passage_a_la_ligne/c1-exo2_main.cpp
+++
@@ -1,6 +0,0 @@
-#include<cstdio>
-int main (){
-    printf("Nkoulou Etoga");
-    printf("Yaoundé");
-    return 0;
-}
